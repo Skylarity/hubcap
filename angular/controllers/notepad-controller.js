a4080c83fdefcdcb5b5cf4e453f80c38a20999dd
Skyler Rexroad <skyrex1095@gmail.com>
2016-02-24 08:48:43 -0700

    Rebranded notepad sections as todo section

--- angular/controllers/notepad-controller.js
+++
@@ -1,3 +0,0 @@
-app.controller("NotepadController", ["$scope", function($scope) {
-    // TODO
-}]);
---
+++ angular/controllers/todo-controller.js
@@ -0,0 +1,3 @@
+app.controller("TodoController", ["$scope", function($scope) {
+    // TODO
+}]);
