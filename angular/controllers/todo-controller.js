694955b57bc54004feae43c9c078f53a61b0010f
Skyler Rexroad <skyrex1095@gmail.com>
2016-02-25 16:50:05 -0700

    Fixed controller issue

--- angular/controllers/todo-controller.js
+++ angular/controllers/todo-controller.js
@@ -1,7 +1,9 @@
 app.controller("TodoController", ["$scope", function($scope) {
     $scope.task = {};
+    $scope.tasks = [{task: "This is a test."}];
 
-    $scope.addTask(task) {
-        console.log(task);
+    $scope.addTask = function(task) {
+        $scope.tasks.push(task);
+        $scope.task = {};
     };
 }]);
