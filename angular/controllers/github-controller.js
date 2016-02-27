56e07f866a9d8c13238efef4e147103104a34944
Skyler Rexroad <skyrex1095@gmail.com>
2016-02-27 13:46:35 -0700

    Trying to get GithubService to work

--- angular/controllers/github-controller.js
+++ angular/controllers/github-controller.js
@@ -1,3 +1,10 @@
-app.controller("GithubController", ["$scope", function($scope) {
-    // TODO
+app.controller("GithubController", ["$scope", "GithubService", function($scope, GithubService) {
+    $scope.feedUrl = "https://github.com/skylarity.atom";
+    $scope.feed = {};
+
+    GithubService.getFeed($scope.feedUrl)
+        .then(function(result) {
+            $scope.feed=result.data.responseData.feed;
+            console.log($scope.feed);
+        })
 }]);
--- angular/services/github-service.js
+++ angular/services/github-service.js
@@ -1,3 +1,5 @@
 app.service("GithubService", function($http) {
-    // TODO
+    this.getFeed = function(url) {
+        return $http.jsonp("https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=" + encodeURIComponent(url));
+    }
 });
