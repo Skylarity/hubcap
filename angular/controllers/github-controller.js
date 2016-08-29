c35bc0a6cb76fb5f6e1c834459ee57e5956e0227
Skyler Rexroad <skyrex1095@gmail.com>
2016-08-29 09:55:20 -0600

    Cleaning up code

--- angular/controllers/github-controller.js
+++ angular/controllers/github-controller.js
@@ -1,9 +1,9 @@
 app.controller("GithubController", ["$scope", "GithubService", function($scope, GithubService) {
-    $scope.feedUrl = "https://github.com/skylarity.atom";
-    $scope.feed = {};
+	$scope.feedUrl = "https://github.com/skylarity.atom";
+	$scope.feed = {};
 
-    GithubService.getFeed($scope.feedUrl)
-        .then(function(result) {
-            $scope.feed = result.data.responseData.feed;
-        })
+	GithubService.getFeed($scope.feedUrl)
+		.then(function(result) {
+			$scope.feed = result.data.responseData.feed;
+		});
 }]);
