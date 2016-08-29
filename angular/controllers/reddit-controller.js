b5fdb9af5675ffd3ca5362e08a7ce7d76dcadc7b
Skyler Rexroad <skyrex1095@gmail.com>
2016-08-29 11:37:41 -0600

    Displaying reddit posts (and can change sub!)

--- angular/controllers/reddit-controller.js
+++ angular/controllers/reddit-controller.js
@@ -1,10 +1,30 @@
 app.controller("RedditController", ["$scope", "RedditService", function($scope, RedditService) {
-	$scope.subreddit = "programmerhumor";
-	$scope.feedUrl = "https://www.reddit.com/r/" + $scope.subreddit + ".json";
+	$scope.subreddit = "";
+	$scope.feedUrl = "";
 	$scope.feed = {};
 
-	RedditService.getFeed($scope.feedUrl)
-		.then(function(result) {
-			$scope.feed = result.data.data;
-		});
+	$scope.getFeed = function(url) {
+		RedditService.getFeed(url)
+			.then(function(result) {
+				$scope.feed = result.data.data;
+			});
+	};
+
+	$scope.setSubreddit = function(subreddit) {
+		if (subreddit.length > 0) {
+			$scope.subreddit = subreddit;
+			$scope.setFeedUrl($scope.subreddit);
+			$scope.getFeed($scope.feedUrl);
+		}
+	};
+
+	$scope.setFeedUrl = function(subreddit) {
+		$scope.feedUrl = "https://www.reddit.com/r/" + subreddit + ".json";
+	};
+
+	if ($scope.subreddit.length === 0) {
+		$scope.subreddit = "ProgrammerHumor";
+		$scope.setFeedUrl($scope.subreddit);
+		$scope.getFeed($scope.feedUrl);
+	}
 }]);
