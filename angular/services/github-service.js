b4f3a075a1b85b207f9a6d5bd6c937bff150ba57
Skyler Rexroad <skyrex1095@gmail.com>
2016-08-29 09:56:56 -0600

    Cleaning more code *scrub scrub*

--- angular/services/github-service.js
+++ angular/services/github-service.js
@@ -1,5 +1,5 @@
 app.service("GithubService", function($http) {
-    this.getFeed = function(url) {
-        return $http.get("https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=" + encodeURIComponent(url));
-    }
+	this.getFeed = function(url) {
+		return $http.get("https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=" + encodeURIComponent(url));
+	};
 });
